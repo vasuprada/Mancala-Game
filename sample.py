2bef0ed0cb870c400d9a7638e075e42cabdc59b4
vasuprada <vasuprada93@gmail.com>
2015-10-09 00:17:26 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -37,36 +37,51 @@ def read_board(file_handle):
                         mancala_two = file_handle.readline().rstrip('\n');
                         mancala_one = file_handle.readline().rstrip('\n');
 
-def greedy(player_no,board_two,board_one):
+def check(player_no,board_two,board_one):
+    global player_no
+    global board_two
+    global board_one
+    l = len(board_one)-1
+    max_eval = board_one[l] - board_two[0]
+    for start in range(0,len(board_one)):
+            greedy(start,player_no,board_two,board_one)
+            if max_eval < board_one[l] - board_two[0]: #eval function
+                    max_eval = board_one[l] - board_two[0]   
+            else:
+                continue
+            
+                                            
+
+def greedy(start,player_no,board_two,board_one):
                                     global player_no
                                     global board_two
                                     global board_one
                                    
                                     if player_no == '1':
-                                        for i in board_one:
-                                            if i>0:
-                                                j = 1
+                                            if board_one[start] > 0:
+                                                j = start+1
                                                 k = len(board_one)-1
                                                 m = len(board_two)-1
-                                                while i!=0:
+                                                while board_one[start]!=0:
                                                     if j <= k:
-                                                        i-=1
+                                                        board_one[start]-=1
                                                         #condition 1- ENDS IN MANCALA-Free turn
-                                                        if (j == k) and (i == 0):
+                                                        if (j == k) and ( board_one[start]== 0):
                                                              board_one[j]+=1
                                                              break
                                                         #condition 2- ENDS IN EMPTY PIT ON SAME SIDE  
-                                                        elif (i == 0) and (board_one[j] == 0):
+                                                        elif (board_one[start] == 0) and (board_one[j] == 0):
                                                             board_one[j] = 0
                                                             #mancala 1 will get beads from both player1 and opp pit
-                                                            board_one[k] = board_one[k] + board_two[k+1]
+                                                            board_one[k] = board_one[k] + board_two[j+1]
+                                                            board_two[j+1]=0
                                                         else:
                                                                 board_one[j]+=1
                                                                 j++
                                                        
                                                     else: # more beads - so adding to player 2's board
                                                             if m!=0:
-                                                                i-=1
+                                                                board_one[start]-=1
                                                                 board_two[m]+=1
                                                                 m--
                                                             else:
@@ -75,13 +90,9 @@ def greedy(player_no,board_two,board_one):
                                                                     j++
                                             
                                                     
-                                         #---BELOW NOT DONE YET---     
-                                         #recursively have to check all moves to find the max eval and then choose the move
-                                            l = len(board_two)-1
-                                            max = board_one[0] - board_two[l] #eval function
-                                            else:
-                                            # if player 2 starts - havent done
-                                            max = board_two[l] - mancala_one[0]
+                                        # ---else part --
+                                        #same thing for when Player_no = '2'
+                                            
 
                                     
                                                             
@@ -115,7 +126,8 @@ def main():
             #output_file_handle.write(str(UCS()) + '\n')
             board_one.append(mancala_one) # player 1 with mancala at the end
             [mancala_two] + board_two     # player 2 with mancala in the beginning
-            greedy(player_no,board_two,board_one)
+            check(player_no,board_two,board_one)
+            #greedy(player_no,board_two,board_one)
             print player_no
             print cut_off_depth 
             for i in board_two:
