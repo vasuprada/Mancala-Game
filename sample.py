e947ad9b75da0014dc3760d32b46a60ef1af9581
vasuprada <vasuprada93@gmail.com>
2015-10-10 17:44:06 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -16,105 +16,112 @@ final_two =""
 
            
 def read_board(file_handle):
-                        global task
-                        global player_no
-                        global cut_off_depth 
-                        global board_two
-                        global board_one
-                        global mancala_two
-                        global mancala_one
+                      global task
+                      global player_no
+                      global cut_off_depth 
+                      global board_two
+                      global board_one
+                      global mancala_two
+                      global mancala_one
        
     
-                        task = int(file_handle.readline().rstrip('\n'))
+                      task = int(file_handle.readline().rstrip('\n'))
                         
-                        player_no = int(file_handle.readline().rstrip('\n'))
-                        cut_off_depth = int(file_handle.readline().rstrip('\n'))
-                        test_two = (file_handle.readline().rstrip('\n').split(' '))
-                        test_one = (file_handle.readline().rstrip('\n').split(' '))
-                        board_two = map(int,test_two)
-                        board_one = map(int,test_one)
-                        mancala_two = int(file_handle.readline().rstrip('\n'));
-                        mancala_one = int(file_handle.readline().rstrip('\n'));
+                      player_no = int(file_handle.readline().rstrip('\n'))
+                      cut_off_depth = int(file_handle.readline().rstrip('\n'))
+                      test_two = (file_handle.readline().rstrip('\n').split(' '))
+                      test_one = (file_handle.readline().rstrip('\n').split(' '))
+                      board_two = map(int,test_two)
+                      board_one = map(int,test_one)
+                      mancala_two = int(file_handle.readline().rstrip('\n'));
+                      mancala_one = int(file_handle.readline().rstrip('\n'));
 
                                         
 def greedy(player_no,board_two,board_one):
-                                    global final_one
-                                    global final_two
+                                 global final_one
+                                 global final_two
                                     
-                                    dummy_one = list(board_one)
-                                    dummy_two = list(board_two)
+                                 dummy_one = list(board_one)
+                                 dummy_two = list(board_two)
 
-                                    for y in dummy_two:
-                                        print y,
-                                    print
-                                    for x in dummy_one:
-                                        print x,
-                                    print
-                                    print
-                                    
-                                    
-                                    if player_no == 1:
-                                        for start in range(0,len(dummy_one)):
-                                            if dummy_one[start] == 0:
-                                                continue
+                                 for y in dummy_two:
+                                            print y,
+                                 print
+                                 for x in dummy_one:
+                                            print x,
+                                 print
+                                 print
+                                 print len(dummy_one) 
+                                 #start = 0
+                                 print 'start before player 1:',start
+                                 if player_no == 1:
+                                            for start in range(0,len(dummy_one)):
+                                                       #print start
+                                                       if dummy_one[start] == 0:
+                                                       print 'start is:',start
+                                                       continue
                                                 
-                                            picked_index = start
-                                            picked_value = dummy_one[picked_index]
-                                            start_from = start+1 #j
-                                            #print start_from
-                                            len_one = len(dummy_one)-1 #k
-                                            #print len_one
-                                            m = len(dummy_two)-1
-                                            #print  m
-                                            max_eval = dummy_one[len_one] - dummy_two[0]
-                                            #print max_eval
-                                            dummy_one[picked_index]= 0
+                                                       picked_index = start
+                                                       picked_value = dummy_one[picked_index]
+                                                       print 'picked value:',picked_value
+                                                       start_from = start+1 #j
+                                                       print 'starting from:',start_from
+                                                       len_one = len(dummy_one)-1 #k
+                                                       print 'length of board 1:',len_one
+                                                       m = len(dummy_two)-1
+                                                       print 'length of board 2:',m
+                                                       max_eval = dummy_one[len_one] - dummy_two[0]
+                                                       print 'max_eval is:',max_eval
+                                                       dummy_one[picked_index]= 0
 
-                                            while picked_value != 0:
-                                                if start_from <= len_one:
-                                                    picked_value -= 1
-                                                    #condition 1- ENDS IN MANCALA-Free turn
-                                                    if (start_from == len_one) and (picked_value == 0):
-                                                        dummy_one[len_one]+= 1
-                                                        #greedy call
-                                                        greedy(player_no,dummy_two,dummy_one)
-                                                        break
+                                                       while picked_value != 0:
+                                                                  picked_value -= 1
+                                                                  if start_from <= len_one:
+                                                                             #picked_value -= 1
+                                                                             #condition 1- ENDS IN MANCALA-Free turn
+                                                                             if (start_from == len_one) and (picked_value == 0):
+                                                                                        dummy_one[len_one]+= 1
+                                                                                        #greedy call
+                                                                                        greedy(player_no,dummy_two,dummy_one)
+                                                                                        break
                                                              
-                                                    #condition 2- ENDS IN EMPTY PIT ON SAME SIDE  
-                                                    elif (picked_value == 0) and (dummy_one[start_from] == 0):
-                                                        #mancala 1 will get beads from both player1 and opp pit
-                                                        dummy_one[start_from] = 1
-                                                        dummy_one[len_one] = dummy_one[len_one] + dummy_one[start_from]+ dummy_two[start_from+1]
-                                                        dummy_one[start_from] = 0
-                                                        dummy_two[start_from+1]= 0
+                                                                             #condition 2- ENDS IN EMPTY PIT ON SAME SIDE  
+                                                                             elif (picked_value == 0) and (dummy_one[start_from] == 0):
+                                                                             #mancala 1 will get beads from both player1 and opp pit
+                                                                                        dummy_one[start_from] = 1
+                                                                                        dummy_one[len_one] = dummy_one[len_one] + dummy_one[start_from]+ dummy_two[start_from+1]
+                                                                                        dummy_one[start_from] = 0
+                                                                                        dummy_two[start_from+1]= 0
                                                         
-                                                        if (max_eval < (dummy_one[len_one] - dummy_two[0])):
-                                                            max_eval = dummy_one[len_one] - dummy_two[0]
-                                                            final_one = list(dummy_one)
-                                                            final_two = list(dummy_two)
-                                                    #conditon 3 - Just dropping beads one by one
-                                                    else:
-                                                        dummy_one[start_from] += 1
-                                                        start_from += 1
+                                                                                        if (max_eval < (dummy_one[len_one] - dummy_two[0])):
+                                                                                        max_eval = dummy_one[len_one] - dummy_two[0]
+                                                                                        final_one = list(dummy_one)
+                                                                                        final_two = list(dummy_two)
+                                                    
+                                                                             #conditon 3 - Just dropping beads one by one
+                                                                             else:
+                                                                                        dummy_one[start_from] += 1
+                                                                                        start_from += 1
                                                        
-                                                else: # more beads - so adding to player 2's board
-                                                    start_from=0
-                                                    if m!=0:
-                                                        dummy_one[start] -= 1
-                                                        dummy_two[m] += 1
-                                                        m -= 1
-                                                        if picked_value == 0:
-                                                            if (max_eval < dummy_one[len_one] - dummy_two[0]):
-                                                                max_eval = dummy_one[len_one] - dummy_two[0]
-                                                                final_one = list(dummy_one)
-                                                                final_two = list(dummy_two)
+                                                                  else: # more beads - so adding to player 2's board
+                                                    
+                                                                             if m!=0:
+                                                                             #dummy_one[start] = 0
+                                                                             dummy_two[m] += 1
+                                                                             m -= 1
+                                                                             print 'm is:',m
+                                                                             if picked_value == 0:
+                                                                                        if (max_eval < dummy_one[len_one] - dummy_two[0]):
+                                                                                                   max_eval = dummy_one[len_one] - dummy_two[0]
+                                                                                                   final_one = list(dummy_one)
+                                                                                                   final_two = list(dummy_two)
 
-                                                    else:
-                                                        dummy_one[start_from] += 1
-                                                        start_from += 1
+                                                                                        else:
+                                                                                                   #dummy_one[start_from] += 1
+                                                                                                   start_from = start + 1
                                             
-                                            dummy_one = list(board_one)
-                                            dummy_two = list(board_two)
+                                                       dummy_one = list(board_one)
+                                                       dummy_two = list(board_two)
                                         
                                     #Else if player 2 
                                             
@@ -151,18 +158,18 @@ def main():
     len_one = len(board_one)-1
     len_two = len(board_two)-1
     if task == 1:
-            #output_file_handle.write(str(UCS()) + '\n')
-            board_one.append(mancala_one) # player 1 with mancala at the end
-            board_two = [mancala_two] + board_two     # player 2 with mancala in the beginning
-            greedy(player_no,board_two,board_one)
-            for i in final_one:
-                print i,
-            print
-            for j in final_two:
-                print j,
-            print
-            print final_two[0]
-            print final_one[len_one]                       
+           #output_file_handle.write(str(UCS()) + '\n')
+           board_one.append(mancala_one) # player 1 with mancala at the end
+           board_two = [mancala_two] + board_two     # player 2 with mancala in the beginning
+           greedy(player_no,board_two,board_one)
+           for i in final_one:
+                      print i,
+           print
+           for j in final_two:
+                      print j,
+           print
+           print final_two[0]
+           print final_one[len_one]                       
             
 
             
