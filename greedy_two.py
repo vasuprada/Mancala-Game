0ae9aaf436652157e18312f2e9c9496c3d5fc169
vasuprada <vasuprada93@gmail.com>
2015-10-17 14:13:34 -0700

    Update greedy_two.py

--- greedy_two.py
+++ greedy_two.py
@@ -37,194 +37,180 @@ def greedy(player_no,board,len_one):
         print
                
             
-  if(player_no == 1):     
+    if(player_no == 1):   
         for start in range(0,len_one):
-                print 'start is',start
-                if dummy[start] == 0:
-                    dummy[start] == 0
-                    print 'start is:',start
-                    continue
+          print 'start is',start
+          if dummy[start] == 0:
+            dummy[start] == 0
+            print 'start is:',start
+            continue
         
-                picked_index = start
-                print 'picked index',picked_index
-                #picked_value = dummy_one[picked_index]
-                picked_value = dummy[picked_index]
-                print 'picked value:',picked_value
-                start_from = start+1 #j
-                print 'starting from:',start_from
-                #len_one = len(dummy_one)-1 #k
-                print 'length of board 1:',len_one
-                m = len(dummy)-1
-                #print 'length of board 2:',m
-                #max_eval = dummy_one[len_one] - dummy_two[0]
-                #print 'max_eval is:',max_eval
-                #dummy_one[picked_index]= 0
-                dummy[picked_index] = 0
+          picked_index = start
+          print 'picked index',picked_index
+          #picked_value = dummy_one[picked_index]
+          picked_value = dummy[picked_index]
+          print 'picked value:',picked_value
+          start_from = start+1 #j
+          print 'starting from:',start_from
+          #len_one = len(dummy_one)-1 #k
+          print 'length of board 1:',len_one
+          m = len(dummy)-1
+          dummy[picked_index] = 0
 
-                while picked_value != 0:
-                        print 'picked_value is:',picked_value              
-                        print 'Outside start_from is :',start_from
-                         
-                        if start_from <= len_one:
-                                picked_value -= 1
-                                #condition 1- ENDS IN MANCALA-Free turn
-                                if (start_from == len_one) and (picked_value == 0):
-                                        dummy[len_one]+= 1
-                                        print 'AT CONDITION 1'
-                                        flag = 0
-                                        flag2 = 0
-                                        
-                                        for u in range(0,len_one):
-                                                if dummy[u]!=0:
-                                                  flag = 1
-                                                  break
-                                                                                                                                                                                                
-                                        for x in range(len_one+2,len(dummy)-1):
-                                                if dummy[x]!=0:
-                                                  flag2 = 1
-                                                  break
+          while picked_value != 0:
+            print 'picked_value is:',picked_value              
+            print 'Outside start_from is :',start_from
+                     
+            if start_from <= len_one:
+              picked_value -= 1
+              #condition 1- ENDS IN MANCALA-Free turn
+              if (start_from == len_one) and (picked_value == 0):
+                dummy[len_one]+= 1
+                print 'AT CONDITION 1'
+                flag = 0
+                flag2 = 0
+                                    
+                for u in range(0,len_one):
+                  if dummy[u]!=0:
+                    flag = 1
+                    break
+                                                                                                                                                                                            
+                for x in range(len_one+2,len(dummy)-1):
+                  if dummy[x]!=0:
+                    flag2 = 1
+                    break
+                                                                                                    
+                if flag == 0:
+                  for n in range(len_one+2,len(dummy)):
+                    dummy_two[len_one+1] += dummy_two[n]
+                    if(dummy[n]!=0):
+                      dummy[n]=0
                                                                                                         
-                                        if flag == 0:
-                                                for n in range(len_one+2,len(dummy)):
-                                                        dummy_two[len_one+1] += dummy_two[n]
-                                                        if(dummy[n]!=0):
-                                                                dummy[n]=0
-                                                                                                            
-                                        if flag2 == 0:
-                                                for v in range(0,len_one):
-                                                        dummy[len_one] += dummy[v]
-                                                        if(dummy[v]!=0):
-                                                                dummy[v]=0
-                                        
-                                        print 'flag 2 and flag',flag2,flag
-                                        print '******************'
+                if flag2 == 0:
+                  for v in range(0,len_one):
+                    dummy[len_one] += dummy[v]
+                    if(dummy[v]!=0):
+                      dummy[v]=0
+                                    
+                print 'flag 2 and flag',flag2,flag
+                print '******************'
 
-                                        for y in dummy:
-                                                print y,
-                                        print
-                                        print '******************'                                                            
-                                       
+                for y in dummy:
+                  print y,
+                print
+                print '******************'                                                            
+                                   
 
-                                        # Either or Both Boards Became empty
-                                        if (flag2 == 0 or flag == 0):
-                                                final = list(dummy)
-                                                #final_two = list(dummy_two)
-                                                break
-                                        #greedy call
-                                        greedy(player_no,dummy,len_one)
+                # Either or Both Boards Became empty
+                if (flag2 == 0 or flag == 0):
+                  final = list(dummy)
+                  break
+                #greedy call
+                greedy(player_no,dummy,len_one)
                                         
              
-                                #condition 2- ENDS IN EMPTY PIT ON SAME SIDE  
-                                elif (picked_value == 0) and (dummy[start_from] == 0):
-                                        #mancala 1 will get beads from both player1 and opp pit
-                                        print 'AT CONDITION 2'
-                                        flag = 0
-                                        flag2 = 0
-                                        dummy[start_from] = 1
-                                        x = start_from + len_one + 2
-                                        #print x
-                                        dummy[len_one] = dummy[len_one] + dummy[start_from]+ dummy[x]
-                                        dummy[start_from] = 0
-                                        dummy[x]= 0
-                                    
-                                        for u in range(0,len_one):
-                                                if dummy[u]!=0:
-                                                  flag = 1
-                                                  break
-                                                                                                                                                                                                
-                                        for x in range(len_one+2,len(dummy)):
-                                                if dummy[x]!=0:
-                                                  flag2 = 1
-                                                  break
-                                                                                                        
-                                        if flag == 0:
-                                                for n in range(len_one+2,len(dummy)):
-                                                        dummy[len_one+1] += dummy[n]
-                                                        if(dummy[n]!=0):
-                                                                dummy[n]=0
-                                                                                                            
-                                        if flag2 == 0:
-                                                for v in range(0,len_one):
-                                                        dummy[len_one] += dummy[v]
-                                                        if(dummy[v]!=0):
-                                                                dummy[v]=0
-                                        print '******************'
-                                        for y in dummy:
-                                                print y,
-                                        print
-                                        print '******************'
-                                        
-                                        
+              #condition 2- ENDS IN EMPTY PIT ON SAME SIDE  
+              elif (picked_value == 0) and (dummy[start_from] == 0):
+                #mancala 1 will get beads from both player1 and opp pit
+                print 'AT CONDITION 2'
+                flag = 0
+                flag2 = 0
+                dummy[start_from] = 1
+                x = start_from + len_one + 2
+                dummy[len_one] = dummy[len_one] + dummy[start_from] + dummy[x]
+                dummy[start_from] = 0
+                dummy[x]= 0
+                            
+                for u in range(0,len_one):
+                  if dummy[u]!=0:
+                    flag = 1
+                    break
+                                                                                                                                                                                        
+                for x in range(len_one+2,len(dummy)):
+                  if dummy[x]!=0:
+                    flag2 = 1
+                    break
+                                                                                                
+                if flag == 0:
+                  for n in range(len_one+2,len(dummy)):
+                    dummy[len_one+1] += dummy[n]
+                    if(dummy[n]!=0):
+                      dummy[n]=0
+                                                                                                    
+                if flag2 == 0:
+                  for v in range(0,len_one):
+                    dummy[len_one] += dummy[v]
+                    if(dummy[v]!=0):
+                      dummy[v]=0
+                
+                print '******************'
+                for y in dummy:
+                  print y,
+                print
+                print '******************'
 
-                                                                                                                                                                                          
-                                        if (max_eval < (dummy[len_one] - dummy[len_one+1])):
-                                                max_eval = dummy[len_one] - dummy[len_one+1]
-                                                final = list(dummy)
-                                                #final_two = list(dummy_two)
+                                                                                                                                                                                  
+                if (max_eval < (dummy[len_one] - dummy[len_one+1])):
+                  max_eval = dummy[len_one] - dummy[len_one+1]
+                  final = list(dummy)
+                                       
 
-                                        elif ((picked_value == 0) and (start_from < len_one)):
-                                                break
-                                        
-                                #conditon 3 - Just dropping beads one by one
-
-                                else:
-                                        print 'AT CONDITION 3'
-                                        dummy[start_from] += 1
-                                        start_from += 1
+              elif ((picked_value == 0) and (start_from < len_one)):
+                break
+                                
+              #conditon 3 - Just dropping beads one by one
+              else:
+                  print 'AT CONDITION 3'
+                  dummy[start_from] += 1
+                  start_from += 1
                
-                        else: # more beads - so adding to player 2's board
-                                print 'In PLayer 2 board side'
-                                print 'Present board condition is '
-                                for y in dummy:
-                                        print y,
-                                print
+            else: # more beads - so adding to player 2's board
+                print 'In PLayer 2 board side'
+                print 'Present board condition is '
+                for y in dummy:
+                  print y,
+                print
                               
 
-                                if(picked_value > 0 and m == len_one + 1):
-                                        print "SKIPPING"
-                                        start_from = 0
+                if(picked_value > 0 and m == len_one + 1):
+                  print "SKIPPING"
+                  start_from = 0
                                                                                      
-                                while(picked_value!= 0 and m > len_one + 1):
-                                        picked_value -= 1
-                                        print 'm is :',m
-                                        dummy[m] += 1
-                                        m -= 1
-                                        print 'm is:',m
-                                        print 'picked_value is',picked_value
+                while(picked_value!= 0 and m > len_one + 1):
+                  picked_value -= 1
+                  print 'm is :',m
+                  dummy[m] += 1
+                  m -= 1
+                  print 'm is:',m
+                  print 'picked_value is',picked_value
                         
                              
-                                if picked_value == 0:
-                                        flag1 = 0
-                                        for u in range(0,len_one):
-                                                if dummy[u]!=0:
-                                                  flag1 = 1
-                                                  break
-                                        if flag1 == 0:
-                                                for n in range(len_one+2,len(dummy)):
-                                                        dummy[len_one+1] += dummy[n]
-                                                        if dummy[n]!=0:
-                                                          dummy[n]=0
-                                                        
-                                        if flagger == 1:# If evals are same, tie breaking on order
-                                                if (max_eval <= dummy[len_one] - dummy[len_one+1]):
-                                                    max_eval = dummy[len_one] - dummy[len_one+1]
-                                                    final = list(dummy)
-                                                    #final_two = list(dummy_two)
-                                        else: 
-                                                if max_eval < dummy[len_one] - dummy[len_one+1]:
-                                                    max_eval = dummy[len_one] - dummy[len_one+1]
-                                                    final = list(dummy)
-                                                    #final_two = list(dummy_two)
+                  if picked_value == 0:
+                    flag1 = 0
+                    for u in range(0,len_one):
+                      if dummy[u]!=0:
+                        flag1 = 1
+                        break
+                                        
+                    if flag1 == 0:
+                      for n in range(len_one+2,len(dummy)):
+                        dummy[len_one+1] += dummy[n]
+                        if dummy[n]!=0:
+                          dummy[n]=0
 
-                                        for y in final:
-                                            print y,
-                                        print
+                    if max_eval < dummy[len_one] - dummy[len_one+1]:
+                      max_eval = dummy[len_one] - dummy[len_one+1]
+                      final = list(dummy)
+                                                   
+                    for y in final:
+                      print y,
+                    print
                                        
-                                        print 'Last start from is : ',start_from
-                                        print 'Last picked value',picked_value                                                                             
+                    print 'Last start from is : ',start_from
+                    print 'Last picked value',picked_value                                                                             
   #player 2 plays                           
-  else:
-    for start in range(len_one+2,len(dummy)):
+    else:
+      for start in range(len_one+2,len(dummy)):
                 print 'start is',start
                 if dummy[start] == 0:
                     dummy[start] == 0
